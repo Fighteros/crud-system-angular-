dda39622e9cd91ce08cdbf40abcf69caf27426df
fighteros <fighteros121212@gmail.com>
2021-09-10 06:19:43 +0200

    commented the model for the current time

--- src/app/components/dialog/dialog.component.ts
+++ src/app/components/dialog/dialog.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { User } from 'src/app/User';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-
+import { faTimes, faEdit, faArrowLeft, faCheck } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-dialog',
   templateUrl: './dialog.component.html',
@@ -9,11 +9,22 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class DialogComponent implements OnInit {
   userList!: User[];
+  opened: boolean = false;
+  faTimes = faTimes;
+  faEdit = faEdit;
+  faArrowLeft = faArrowLeft;
+  faCheck = faCheck;
   constructor(@Inject(MAT_DIALOG_DATA) public data: User[]) {
   }
 
   ngOnInit(): void {
   }
 
+  userEdit(user: User) {
+    this.opened = true;
+  }
 
+  saveEdit(user: User) {
+    this.opened = false;
+  }
 }
